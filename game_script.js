9bd65baeca651d06b77a5f6a6482e14161b4e22a
chawlajay <chawlajay111@gmail.com>
2021-01-19 11:37:06 +0530

    added click eventListener for each cells of grid

--- game_script.js
+++ game_script.js
@@ -6,17 +6,30 @@ let squares=Array.from(document.querySelectorAll('.cell'));
 
 const message=document.getElementById("message");
 
+const GLOWTEXT="glow";
 function add_symbol(element){
     if(count%2)
     {
         //player 2 - X
         element.innerHTML="X";
-        count++;
     }
     else
     {
         //player 1 - O
         element.innerHTML="O";
-        count++;
+    }
+    element.classList.toggle(GLOWTEXT);
+    count++;
+}
+
+matrix.addEventListener('click',(event)=>{
+let clicked_cell_element = event.target;
+
+if(clicked_cell_element.classList.contains('cell'))
+{
+    if(clicked_cell_element.innerHTML=="")
+    {
+        add_symbol(clicked_cell_element);
     }
 }
+});
