c1a5b9a2e78019af5c289cd4cb42a98099639fc8
chawlajay <chawlajay111@gmail.com>
2021-01-19 14:41:14 +0530

    add check winner function to find and show winner

--- game_script.js
+++ game_script.js
@@ -14,6 +14,67 @@ function display_message(mes){
     message.innerHTML=mes;
 }
 
+function check_winner(){
+    let win=0;
+    //check horizontal cells
+    for(let i=0;i<9;i+=3)
+    {
+        if(squares[i].innerHTML==squares[i+1].innerHTML && squares[i+1].innerHTML==squares[i+2].innerHTML)
+        {
+            if(squares[i].innerHTML=='O')
+            {
+                display_message("Player 1 WON the game ! *Congratulations*");
+            }
+            else if(squares[i].innerHTML=='X')
+            {
+                display_message("Player 2 WON the game ! *Congratulations*");
+            }
+        }
+    }
+
+    //check vertical cells
+    for(let i=0;i<3;i++)
+    {
+        if(squares[i].innerHTML==squares[i+3].innerHTML && squares[i+3].innerHTML==squares[i+6].innerHTML)
+        {
+            if(squares[i].innerHTML=='O')
+            {
+                display_message("Player 1 WON the game ! *Congratulations*");
+            }
+            else if(squares[i].innerHTML=='X')
+            {
+                display_message("Player 2 WON the game ! *Congratulations*");
+            }
+        }
+    }
+
+    //check Primary Diagonal
+    if(squares[0].innerHTML==squares[4].innerHTML && squares[4].innerHTML==squares[8].innerHTML)
+    {
+        if(squares[0].innerHTML=='O')
+            {
+                display_message("Player 1 WON the game ! *Congratulations*");
+            }
+            else if(squares[0].innerHTML=='X')
+            {
+                display_message("Player 2 WON the game ! *Congratulations*");
+            }
+    }
+
+    //check Secondary Diagonal
+    if(squares[2].innerHTML==squares[4].innerHTML && squares[4].innerHTML==squares[6].innerHTML)
+    {
+        if(squares[2].innerHTML=='O')
+            {
+                display_message("Player 1 WON the game ! *Congratulations*");
+            }
+            else if(squares[2].innerHTML=='X')
+            {
+                display_message("Player 2 WON the game ! *Congratulations*");
+            }
+    }
+}
+
 function add_symbol(element){
     if(count%2)
     {
@@ -28,6 +89,7 @@ function add_symbol(element){
         display_message("Player 2's Move");
     }
     element.classList.toggle(GLOWTEXT);
+    check_winner();
     count++;
 }
 
